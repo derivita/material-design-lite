971e663fa5741ec58ea1e5c5569dbcac9fbd6031
Jake Lauritsen <jake.lauritsen@derivita.com>
2024-07-19 10:51:30 -0700

    Clean linter

--- src/layout/layout.js
+++ src/layout/layout.js
@@ -227,7 +227,6 @@
         // prevent scrolling in drawer nav
         evt.preventDefault();
       } else {
-        // prevent other keys2a79247898d1680570c03d8b1cdc5eaf184386372a79247898d1680570c03d8b1cdc5eaf184386372a79247898d1680570c03d8b1cdc5eaf18438637
         return;
       }
     }
